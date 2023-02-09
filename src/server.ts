2c6c37150cdf5631733a03641580b629a309c70e
TakaoIsTheBest <saheb1379@gmail.com>
2023-02-09 15:05:59 +0330

    remove console.log

--- src/server.ts
+++ src/server.ts
@@ -5,8 +5,6 @@ dotenv.config();
 
 const app = express();
 
-console.log(process.env.port);
-
 const PORT = process.env.port || 5000;
 app.listen(PORT, () => {
 	console.log(`⚡[SERVER]: Server running on http://localhost:${PORT}`);
