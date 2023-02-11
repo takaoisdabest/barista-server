920ac3fde6b4318c6053ec07dd7d0876d8b0c3e1
TakaoIsTheBest <saheb1379@gmail.com>
2023-02-11 15:13:32 +0330

    add cors and

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import cors from "cors";
 
 // Routes
 import userRoutes from "./routes/userRoutes";
@@ -7,6 +8,10 @@ import userRoutes from "./routes/userRoutes";
 dotenv.config();
 
 const app = express();
+
+app.use(express.json({ limit: "30mb" }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
 app.use("/api/users", userRoutes);
 
