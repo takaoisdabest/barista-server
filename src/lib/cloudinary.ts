c8f5525ee9ed3421b6f89e5bf154e261e77f663e
TakaoIsTheBest <saheb1379@gmail.com>
2023-02-13 08:56:50 +0330

    add different sizes for cropping

--- src/lib/cloudinary.ts
+++ src/lib/cloudinary.ts
@@ -8,8 +8,13 @@ cloudinary.config({
 	api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
-const uploadImage = async (image: string) => {
+const uploadImage = async (image: string, type?: "profile" | "banner" | "thumbnail" | "item") => {
 	const public_id = randomUUID();
+	const size = {
+		profile: { width: 75, height: 75 },
+		banner: { width: 1920, height: 256 },
+		thumbnail: { width: 256, height: 144 }
+	};
 
 	// Upload
 	const res = await cloudinary.uploader.upload(image, { public_id, unique_filename: true });
@@ -17,13 +22,35 @@ const uploadImage = async (image: string) => {
 	console.log(res);
 	console.log(res.secure_url);
 
-	// Generate
-	const url = cloudinary.url(public_id, {
-		width: 75,
-		height: 75,
-		Crop: "fill"
-	});
+	// Generate item image without cropping
+	if (type === "item") {
+		const url = cloudinary.url(public_id);
+		return url;
+	}
 
+	// Generate images with cropping
+	let width: number;
+	let height: number;
+	switch (type) {
+		case "profile":
+			width = size.profile.width;
+			height = size.profile.height;
+			break;
+		case "banner":
+			width = size.banner.width;
+			height = size.banner.height;
+			break;
+		case "thumbnail":
+			width = size.thumbnail.width;
+			height = size.thumbnail.height;
+			break;
+		default:
+			width = size.profile.width;
+			height = size.profile.height;
+			break;
+	}
+
+	const url = cloudinary.url(public_id, { width, height, Crop: "fill" });
 	return url;
 };
 
