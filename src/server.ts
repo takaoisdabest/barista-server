b2b32af6d5a50006fe1080dba58cd6f3ae1ebe44
TakaoIsTheBest <saheb1379@gmail.com>
2023-02-13 09:06:20 +0330

    add error handling middleware

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,9 @@ import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
 
+// Middlewares
+import errorHandler from "./middleware/errorHandler";
+
 // Routes
 import userRoutes from "./routes/userRoutes";
 
@@ -12,6 +15,8 @@ const app = express();
 app.use(express.json({ limit: "30mb" }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use(errorHandler);
 
 app.use("/api/users", userRoutes);
 
